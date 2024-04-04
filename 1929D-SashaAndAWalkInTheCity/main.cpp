6c90959df05ae0360ee4460a6975035090a5e337
Hoa Vu <hoavu.tr@gmail.com>
2024-04-03 18:17:59 -0700

    delete unnecessary files

--- 1929D-SashaAndAWalkInTheCity/main.cpp
+++ 1929D-SashaAndAWalkInTheCity/main.cpp
@@ -4,32 +4,23 @@
 
 using namespace std;
 
-const long MOD = 998244353;
+const long long MOD = 998244353;
 vector<vector<long>> adj;
-vector<long> A, B;
+vector<long long> A, B;
 
 void dfs(long u, long p) {
-    long n_children = 0;
+    A[u] = 1;
+    B[u] = 0;
+    int n_children = 0;
     for (auto v : adj[u]) {
         if (v == p) continue;
         dfs(v, u);
+        A[u] = (A[u] * (A[v] + 1)) % MOD;
+        B[u] = (B[u] + (A[v] - 1)) % MOD;
         n_children++;
     }
 
-    if (n_children == 0) {
-        A[u] = 1;
-        B[u] = 0;
-    } else {
-        long x = 1, y = 0;
-        for (auto v : adj[u]) {
-            if (v == p) continue;
-            x = (x * (A[v] + 1)) % MOD;
-            y = (y + (A[v] - 1)) % MOD;
-        }
-        A[u] = x % MOD;
-        B[u] = (y + n_children) % MOD;
-    }    
-    //cout << "A[" << u << "] = " << A[u] << ", B[" << u << "] = " << B[u] << endl;
+    B[u] = (B[u] + n_children) % MOD;
 }
 
 int main() {
@@ -37,15 +28,9 @@ int main() {
     long n_tests;
     cin >> n_tests;
 
-    while(n_tests > 0) {
+    while(n_tests-- > 0) {
         long n;
         cin >> n;
-
-        if (n == 0) {
-            cout << 1 << endl;
-            n_tests--;
-            continue;
-        }
 
         adj.assign(n + 1, vector<long>());  
         A.assign(n + 1, 0);
@@ -58,14 +43,14 @@ int main() {
             adj[v].push_back(u);
         }
 
-        dfs(1, -1);
-        long ans = A[1] + 1;
+        dfs(1, 0);
+        long long ans = A[1] + 1;
         for (long i = 1; i <= n; i++) {
             ans = (ans + B[i]) % MOD;
         }
         
-        cout << ans % MOD << endl;
-        n_tests--;
+        cout << (ans % MOD + MOD) % MOD << endl;
     }
+    
     return 0;
 }
