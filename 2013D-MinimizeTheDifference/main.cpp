976cf22bc3eecc1182e5f7e54f650bd981421691
Hoa Vu <hvutrong@gmail.com>
2024-10-05 03:50:13 -0700

    minimize diff

--- 2013D-MinimizeTheDifference/main.cpp
+++ 2013D-MinimizeTheDifference/main.cpp
@@ -1,25 +1,62 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <stack>
+#include <climits>
 
 using namespace std;
+typedef long long ll;
 
-typedef unsigned long long ull;
 
-// Difference Array Functions
+int main() {
+    int num_tests;
+    cin >> num_tests;
 
-void update_diff_array(vector<ull>& diff, int i, int j, ull val) {
-    // Add val to all elements in the range [i, j]
-    diff[i] += val;
-    diff[j+1] -= val;
-}
+    while (num_tests--) {
+        int n;
+        cin >> n;
 
-vector<ull> reconstruct_array(const vector<ull>& diff) {
-    // Reconstruct the original array from the difference array
-    vector<ull> arr(diff.size());
-    arr[0] = diff[0];
-    for (int i = 1; i < diff.size(); i++) {
-        arr[i] = arr[i-1] + diff[i];
+        vector<ll> a(n);
+        stack<pair<ll, ll>> s;
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        s.push({a[0], 1});
+        for (int i = 1; i < n; i++) {
+            if (a[i] == s.top().first) {
+                s.top().second++;
+            } else if (a[i] > s.top().first) {
+                s.push({a[i], 1});
+            } else {
+                ll sum = a[i];
+                int num_elements = 1;
+                
+                while (s.size() > 0 && sum / num_elements < s.top().first) {
+                    sum += s.top().first * s.top().second;
+                    num_elements += s.top().second;
+                    s.pop();
+                }
+
+                ll d = sum / num_elements;
+                ll r = sum % num_elements;
+
+                s.push({d, num_elements - r});
+                if (r) s.push({d + 1, r});
+            }
+        }
+
+        ll min_val = LLONG_MAX;
+        ll max_val = LLONG_MIN;
+
+        while (!s.empty()) {
+            min_val = min(min_val, s.top().first);
+            max_val = max(max_val, s.top().first);
+            s.pop();
+        }
+
+        cout << max_val - min_val << endl;
     }
-    return arr;
+
+    return 0;
 }
