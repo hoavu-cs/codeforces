e32dd7b1cd6d6e9f470ae673c1f6e40e589eb5f8
Hoa Vu <hvutrong@gmail.com>
2024-10-24 23:44:14 -0700

    improve

--- 2013D-MinimizeTheDifference/main.cpp
+++ 2013D-MinimizeTheDifference/main.cpp
@@ -1,61 +1,54 @@
 #include <iostream>
 #include <vector>
-#include <stack>
 #include <climits>
 
 using namespace std;
 typedef long long ll;
-
 
 int main() {
     int num_tests;
     cin >> num_tests;
 
     while (num_tests--) {
+        // reading the next test case
         int n;
         cin >> n;
 
         vector<ll> a(n);
-        stack<pair<ll, ll>> s;
+        vector<pair<ll, ll>> s; 
 
         for (int i = 0; i < n; i++) {
             cin >> a[i];
         }
+        
+        // This is where the algorithm starts. 
+        s.push_back({a[0], 1});
 
-        s.push({a[0], 1});
         for (int i = 1; i < n; i++) {
-            if (a[i] == s.top().first) {
-                s.top().second++;
-            } else if (a[i] > s.top().first) {
-                s.push({a[i], 1});
+            if (a[i] == s.back().first) {
+                s.back().second++;
+            } else if (a[i] > s.back().first) {
+                s.push_back({a[i], 1});
             } else {
                 ll sum = a[i];
                 int num_elements = 1;
                 
-                while (s.size() > 0 && sum / num_elements < s.top().first) {
-                    sum += s.top().first * s.top().second;
-                    num_elements += s.top().second;
-                    s.pop();
+                // Simulate stack pop and aggregate elements
+                while (!s.empty() && sum / num_elements < s.back().first) {
+                    sum += s.back().first * s.back().second;
+                    num_elements += s.back().second;
+                    s.pop_back(); // pop from vector
                 }
 
                 ll d = sum / num_elements;
                 ll r = sum % num_elements;
 
-                s.push({d, num_elements - r});
-                if (r) s.push({d + 1, r});
+                s.push_back({d, num_elements - r});
+                if (r) s.push_back({d + 1, r});
             }
         }
-
-        ll min_val = LLONG_MAX;
-        ll max_val = LLONG_MIN;
-
-        while (!s.empty()) {
-            min_val = min(min_val, s.top().first);
-            max_val = max(max_val, s.top().first);
-            s.pop();
-        }
-
-        cout << max_val - min_val << endl;
+        // Output the difference between the largest and smallest elements. This is where I replace the loop in my video.
+        cout << s.back().first - s.front().first << endl;
     }
 
     return 0;
