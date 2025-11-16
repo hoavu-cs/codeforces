a0949acfc613ac93e1d455ac75f2b26c9da47904
Hoa Vu <hvutrong@gmail.com>
2025-11-15 20:05:41 -0800

    against the difference

--- 2135A-AgainstTheDifference/main.cpp
+++ 2135A-AgainstTheDifference/main.cpp
@@ -4,18 +4,20 @@
 
 using namespace std;
 
-int binary_search(const vector<int>& vec, int target) {
-    int left = 0, right = vec.size() - 1;
+int binary_search(const vector<int>& v, int target) {
+    int left = 0, right = v.size() - 1;
+
     while (left <= right) {
         int mid = left + (right - left) / 2;
-        if (vec[mid] == target) {
+        if (v[mid] == target) {
             return mid;
-        } else if (vec[mid] < target) {
+        } else if (v[mid] < target) {
             left = mid + 1;
         } else {
             right = mid - 1;
         }
     }
+
     return -1;
 }
 
@@ -26,45 +28,42 @@ int main() {
     while (T--) {
         int n;
         cin >> n;
+        
         vector<int> a(n);
-        vector<int> dp(n, 0);
-        map<int, vector<int>> positions; // positions[value] = list of indices with that value
-
+        map<int, vector<int>> p;
+        
         for (int i = 0; i < n; ++i) {
             cin >> a[i];
-            positions[a[i]].push_back(i);
-        }
+            p[a[i]].push_back(i);
+        }  
 
+        vector<int> dp(n, 0);
         dp[0] = (a[0] == 1) ? 1 : 0;
-
+        /*
+            dp[i] = length of the longest neat subsequence in a[0..i]
+            the best subsequence either contains a[i] or it doesn't
+            1. if it doesn't contain a[i], dp[i] = dp[i-1]
+            2. if it contains a[i], let x = a[i]
+            we need to find index j where the current x-block begins
+            dp[i] = max(dp[i], dp[j-1] + x).
+            Note that j = p[x][i - x + 1].
+        */
         for (int i = 1; i < n; ++i) {
-            dp[i] = dp[i - 1];
-            int j = binary_search(positions[a[i]], i);
-            
-            /*
-                We have 2 cases:
-                1. The best neat subsequence in a[1..n] contains a[i]
-                In this case, we need to look for the index of the previous occurrence of a[i] that is at position j - a[i] + 1 to cover the last block.
-                Then, the total length will be dp[prev_index - 1] + a[i].
 
-                2. The best neat subsequence in a[1..n] does not contain a[i]
-                In this case, dp[i] = dp[i - 1]
-            */
-
-            if (j >= a[i] - 1) {
-                int prev_index = positions[a[i]][j - a[i] + 1];
-                if (prev_index == 0) {
-                    dp[i] = max(dp[i], a[i]);
+            dp[i] = dp[i-1];
+            int x = a[i];
+            int j = binary_search(p[x], i);
+            if (j >= a[i] - 1) { // ensure that we have enough elements for an x-block
+                int prev_idx = p[x][j - x + 1];
+                if (prev_idx == 0) {
+                    dp[i] = max(dp[i], x);
                 } else {
-                    dp[i] = max(dp[i], dp[prev_index - 1] + a[i]); 
+                    dp[i] = max(dp[i], dp[prev_idx - 1] + x);
                 }
             }
         }
-
-        cout << dp[n - 1] << endl;
-
+        cout << dp[n-1] << endl;
     }
-
 
     return 0;
 }
